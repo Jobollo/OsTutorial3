d67831e71b5e59f05cb76c083272f2cc14109abf
DragoniteX1 <joshua.pineda1@uoit.net>
2018-02-04 18:36:47 -0500

    Added question2.c
    Requires question2.txt to work.

---
+++ question2.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+
+    int numbers[10];
+    FILE *q2file;
+    q2file = fopen("question2.txt", "r");
+    int num = 0;
+    int n = 0;
+    //fscanf (q2file, "%d", &num);
+    while (!feof(q2file))
+    {
+        fscanf (q2file, "%d", &num);
+        numbers[n] = num;
+        n += 1;
+ 
+    }
+
+    for (int i = 0; i < 10; i ++)
+    {
+        printf("%d\n", numbers[i]);
+    }
+
+    fclose(q2file);
+
+}
